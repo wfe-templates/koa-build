a8c8a87e43f9c50ac92587191aee16c198b43a2f
busyhe <525118368@qq.com>
2018-12-12 18:21:32 +0800

    初始化测试

--- meta.js
+++ meta.js
@@ -1,44 +1,46 @@
 module.exports = {
-    name: {
-        type: 'string',
-        required: true,
-        message: 'Project name',
-    },
-    description: {
-        type: 'string',
-        required: false,
-        message: 'Project description',
-        default: 'A Vue.js project',
-    },
-    author: {
-        type: 'string',
-        message: 'Author',
-    },
-    db: {
-        type: 'confirm',
-        message: 'Use db to your server',
-    },
-    dbConfig: {
-        when: 'db',
-        type: 'list',
-        message: 'select db',
-        choices: [
-            {
-                name: 'Mongodb',
-                value: 'mongodb',
-                short: 'mongodb',
-            },
-            {
-                name: 'Mysql',
-                value: 'mysql',
-                short: 'mysql',
-            },
-            {
-                name: 'none',
-                value: 'none',
-                short: 'none',
-            }
-        ]
+    prompts: {
+        name: {
+            type: 'string',
+            required: true,
+            message: 'Project name',
+        },
+        description: {
+            type: 'string',
+            required: false,
+            message: 'Project description',
+            default: 'A Vue.js project',
+        },
+        author: {
+            type: 'string',
+            message: 'Author',
+        },
+        db: {
+            type: 'confirm',
+            message: 'Use db to your server',
+        },
+        dbConfig: {
+            when: 'db',
+            type: 'list',
+            message: 'select db',
+            choices: [
+                {
+                    name: 'Mongodb',
+                    value: 'mongodb',
+                    short: 'mongodb',
+                },
+                {
+                    name: 'Mysql',
+                    value: 'mysql',
+                    short: 'mysql',
+                },
+                {
+                    name: 'none',
+                    value: 'none',
+                    short: 'none',
+                }
+            ]
+        }
     },
     filters: {
         // 'db/**/*': "use_db",
